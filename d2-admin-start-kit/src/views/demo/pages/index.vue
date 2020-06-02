1c996d82edec7ec46abce2bee5553d73ffc28633
李东兵 <634163114@qq.com>
2020-06-03 00:02:59 +0800

    基于d2Admin开源而开发的系统

---
+++ d2-admin-start-kit/src/views/demo/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <d2-container better-scroll>
+    <template slot="header">
+      this.$store.dispatch('d2admin/menu/asideCollapseSet', true)
+    </template>
+    Hello World
+    <template slot="footer">footer</template>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  mounted () {
+    debugger
+    // console.log(this.$store.dispatch('d2admin/menu/asideCollapseSet', false))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
